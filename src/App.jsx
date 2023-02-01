f7e243e40af8a2b7b8eaa44bbeaee1c6a5cad15c
Megan McBride <meganambermcbride@gmail.com>
2023-01-31 21:44:45 -0500

    Add missed call count state for badge count

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import ReactDOM from "react-dom/client";
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Calls from "./pages/Calls.jsx";
 import { QueryClient, QueryClientProvider } from "react-query";
+import Calls from "./pages/Calls.jsx";
+import BottomNav from "./components/BottomNav.jsx";
 // import Status from "./components/Status";
 // import Profile from "./pages/Profile";
 // import Keypad from "./pages/Keypad";
 const queryClient = new QueryClient();
 
 export default function App() {
+  const [ missedCount, setMissedCount ] = useState(null)
+  
   return (
     <div className="container">
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Calls />}></Route>
+            <Route path="/" element={<Calls setMissedCount={setMissedCount} />}></Route>
           </Routes>
+            <div className="bottomNav">
+              <BottomNav missedCount={missedCount}/>
+            </div>
         </BrowserRouter>
       </QueryClientProvider>
     </div>
