9f65276c34e1cf086186c32588d28ab02a3c17b3
Megan McBride <meganambermcbride@gmail.com>
2023-02-01 22:54:50 -0500

    Fix missed call count bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,34 @@ import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Calls from "./pages/Calls.jsx";
-import BottomNav from "./components/BottomNav.jsx";
-// import Status from "./components/Status";
-// import Profile from "./pages/Profile";
-// import Keypad from "./pages/Keypad";
+
 const queryClient = new QueryClient();
 
 export default function App() {
-  const [ missedCount, setMissedCount ] = useState(null)
-  
+  const checkMissedCalls = () => {
+    if (!window.localStorage.getItem("missCallCount")) {
+      return undefined;
+    }
+    return JSON.parse(window.localStorage.getItem("missCallCount"));
+  };
+  const [count, setCount] = useState(() => checkMissedCalls());
+  const [missedCallCount, setMissedCallCount] = useState(count);
+
   return (
     <div className="container">
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Calls setMissedCount={setMissedCount} />}></Route>
+            <Route
+              path="/"
+              element={
+                <Calls
+                  missedCallCount={missedCallCount}
+                  setMissedCallCount={setMissedCallCount}
+                />
+              }
+            ></Route>
           </Routes>
-            <div className="bottomNav">
-              <BottomNav missedCount={missedCount}/>
-            </div>
         </BrowserRouter>
       </QueryClientProvider>
     </div>
