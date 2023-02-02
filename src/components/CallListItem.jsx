b651e27c7141b63fc20710494d575bf2dab9695b
Megan McBride <meganambermcbride@gmail.com>
2023-02-01 22:59:27 -0500

    Add conditional rendering for call icons if call is missed, inbound or outbound

--- src/components/CallListItem.jsx
+++ src/components/CallListItem.jsx
@@ -1,31 +1,48 @@
 import React from "react";
-import { VscCallIncoming, VscCallOutgoing, VscInfo } from "react-icons/vsc";
+import { VscCallIncoming, VscCallOutgoing } from "react-icons/vsc";
+import "../css/CallListItem.css";
 
 const CallListItem = (props) => {
   const fullDate = new Date(props.created_at);
   const date = fullDate.toLocaleString().slice(0, 9);
-  const time =
-    fullDate.toLocaleTimeString().slice(0, 5) +
-    fullDate.toLocaleTimeString().slice(8);
+  const time = fullDate.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  +fullDate.toLocaleTimeString().slice(8);
+
   return (
     <div>
       <div className="date">{date}</div>
-      <div className="call-container" onClick={() => {props.setDetailOpen(true); props.setCallDetails(props); }}>
+      <div
+        className="call-container"
+        onClick={() => {
+          props.setDetailOpen(true);
+          props.setCallDetails(props);
+        }}
+      >
         <div className="call-icon">
-          {props.direction === "inbound" && <VscCallIncoming size="20px" />}
+          {props.direction === "inbound" && props.call_type !== "missed" && (
+            <VscCallIncoming size="20px" />
+          )}
           {props.direction === "outbound" && <VscCallOutgoing size="20px" />}
         </div>
         <div className="call-info-mid">
-          <p className="caller"> {props.from} </p>
-          <p className="via">tried to call from {props.via}</p>
+          <p
+            className={props.call_type === "missed" ? "callerMissed" : "caller"}
+          >
+            {props.from === undefined ? "Unknown" : props.from}
+          </p>
+          <p className="via">
+            called from {props.via === undefined ? "Unknown" : props.via}
+          </p>
         </div>
         <div className="call-info-right">
-
           <p className="time">{time}</p>
         </div>
       </div>
     </div>
-    
   );
 };
 
